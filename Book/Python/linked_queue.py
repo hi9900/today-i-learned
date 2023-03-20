6d1f8cf7874456d21be17d952711a1db1925b58b
HY <9hiyoung@gmail.com>
2023-03-20 23:34:15 +0900

    ADD: queue

---
+++ Book/Python/linked_queue.py
@@ -0,0 +1,50 @@
+class Node(object):
+    def __init__(self, value=None, pointer=None):
+        self.value = value
+        self.pointer = None
+
+class LinkedQueue(object):
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.count = 0
+
+    def isEmpty(self):
+        return not bool(self.head)
+
+    def dequeue(self):
+        if self.head:
+            value = self.head.value
+            self.head = self.head.pointer
+            self.count -= 1
+            return value
+        else:
+            print("Queue is empty")
+
+    def enqueue(self, value):
+        node = None(value)
+        if not self.head:
+            self.head = node
+            self.tail = node
+        else:
+            if self.tail:
+                self.tail.pointer = node
+            self.tail = node
+        self.count += 1
+
+    def size(self):
+        return self.count
+            
+    def peek(self):
+        return self.head.value
+    
+    def print(self):
+        node = self.head
+        while node:
+            print(node.value, end=" ")
+            node = node.pointer
+        print()
+    
+
+        
+        
---
+++ Book/Python/queue_.py
@@ -0,0 +1,40 @@
+class Queue(object):
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return not bool(self.items)
+
+    def enqueue(self, item):
+        self.items.insert(0, item)
+
+    def dequeue(self):
+        value = self.items.pop()
+        if value is not None:
+            return value
+        else:
+            print("Queue is empty")
+
+    def size(self):
+        return len(self.items)
+
+    def peek(self):
+        if self.items:
+            return self.items[-1]
+        else:
+            print("Queue is empty")
+    
+    def __repr__(self) -> str:
+        return repr(self.items)
+
+if __name__ == "__main__":
+    queue = Queue()
+    print(f"큐가 비었나요? {queue.isEmpty()}")
+    print("큐에 숫자 0 ~ 9 추가")
+    for i in range(10):
+        queue.enqueue(i)
+    print(f"큐 크기: {queue.size()}")
+    print(f"peek: {queue.peek()}")
+    print(f"dequeue: {queue.dequeue()}")
+    print(f"peek: {queue.peek()}")
+    print(queue)
