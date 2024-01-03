7d0d5f424919b27ab0b35cac70ded283cab062d3
구희영 <9hiyoung@gmail.com>
2024-01-03 17:28:53 +0900

    DELETE: ect 폴더 삭제

--- ect/self-project/con01/rep02/Query.sql
+++
@@ -1,6 +0,0 @@
-SELECT * FROM "PRJ1"."TBL_USER";
-
-SELECT LAST_VALUE FROM "PRJ1"."SEQ_USER";
-
-INSERT INTO "PRJ1"."TBL_USER"(user_seq, user_id, user_name, user_pass, user_level)
-VALUES (NEXTVAL('"PRJ1"."SEQ_USER"'),'id1','사용자이름1', 'pass', '1');
